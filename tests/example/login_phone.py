f98384c8d6f2cf5d0eb712c896adf1b827a7db4b
shizeying <shizeying@yupaowang.com>
2025-05-14 23:50:03 +0800

    add empty implementation for sign_function to prevent None assignment

--- tests/example/login_phone.py
+++ tests/example/login_phone.py
@@ -1,20 +1,17 @@
 import datetime
 import json
-from http.cookiejar import Cookie
 from time import sleep
 
 import qrcode
 import requests
-from playwright.sync_api import sync_playwright
-
-from xhs import DataFetchError, XhsClient
-
+from xhs import XhsClient
 
 
 def sign(uri, data=None, a1="", web_session=""):
     # 填写自己的 flask 签名服务端口地址
     res = requests.post("http://154.89.148.31:5005/sign",
                         json={"uri": uri, "data": data, "a1": a1, "web_session": web_session})
+    print(res)
     signs = res.json()
     return {
         "x-s": signs["x-s"],
@@ -23,7 +20,6 @@ def sign(uri, data=None, a1="", web_session=""):
 
 
 if __name__ == '__main__':
-    Cookie="a1=187d2defea8dz1fgwydnci40kw265ikh9fsxn66qs50000726043;webId=ba57f42593b9e55840a289fa0b755374;gid.sign=PSF1M3U6EBC/Jv6eGddPbmsWzLI=;gid=yYWfJfi820jSyYWfJfdidiKK0YfuyikEvfISMAM348TEJC28K23TxI888WJK84q8S4WfY2Sy;acw_tc=0a50864f17472147403221376e3e8d3210955cf1ba215b86d9874183b8b0bf;web_session=0400698cf3028b57e499cf29113a4bbf5db5a3"
     xhs_client = XhsClient(sign=sign)
     print(datetime.datetime.now())
     qr_res = xhs_client.get_qrcode()
